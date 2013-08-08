52336dd7b816f8d410d79d6fb35acdfb1070b594
Juliana Seng <hexcone@gmail.com>
2013-08-08 23:07:22 +0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nypsu::Application.config.secret_token = 'f495bc4ccb6d32c2f22d9c2d3c6286733c3429287a5ec892882134147647572ca930933898223bbbde4614e48aa935565ecb6d19ba5bc8c0bd3ae5200c3626d9'
